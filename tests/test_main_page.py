d62e3c318567e731e4112594af607e88c05c5bde
Julia Pozdnyakova <feainnemedd@yandex.ru>
2026-04-18 21:07:23 +0300

    Fix: separate tests for atomicity and add counter verification

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,93 +1,61 @@
+import allure
+import pytest
 from page_objects.main_page import MainPage
 from page_objects.feed_page import FeedPage
-from conftest import *
-import allure
-
 
 class TestMainPage:
 
     @allure.title('Проверка перехода по клику на "Конструктор"')
-    @allure.description('''
-    Проверка навигации через кнопку "Конструктор" в хедере:
-    1. Перейти в раздел "Лента заказов"
-    2. Кликнуть на кнопку "Конструктор" в хедере
-    3. Проверить возврат на главную страницу конструктора
-    4. Убедиться в отображении заголовка "Соберите бургер"
-    ''')
     def test_navigate_to_constructor_success(self, driver):
         main_page = MainPage(driver)
         feed_page = FeedPage(driver)
         
-        # Переходим в ленту заказов для тестирования навигации
         main_page.click_header_feed_button()
-        assert feed_page.get_text_on_title_of_orders_list() == 'Лента заказов'
+        # Проверяем, что перешли в ленту заказов
+        assert feed_page.get_text_on_title_of_orders_list() == "Лента заказов"
         
-        # Возвращаемся в конструктор через кнопку в хедере
         main_page.click_on_button_constructor()
-        assert 'Соберите бургер' in main_page.get_text_on_title_of_constructor()
+        # Проверяем возврат на главную (Конструктор)
+        assert main_page.get_text_on_title_of_constructor() == "Соберите бургер"
 
-    @allure.title('Проверка перехода по клику на "Ленту заказов"')
-    @allure.description('''
-    Проверка перехода в раздел "Лента заказов":
-    1. Перейти на главную страницу
-    2. Кликнуть на кнопку "Лента заказов" в хедере
-    3. Проверить переход в раздел ленты заказов
-    4. Убедиться в отображении заголовка раздела
-    ''')
+    @allure.title('Проверка перехода по клику на "Лента заказов"')
     def test_navigate_to_order_history_success(self, driver):
         main_page = MainPage(driver)
         feed_page = FeedPage(driver)
+        
         main_page.click_header_feed_button()
-        assert feed_page.get_text_on_title_of_orders_list() == 'Лента заказов'
+        assert feed_page.get_text_on_title_of_orders_list() == "Лента заказов"
 
-    @allure.title('Проверка отображения окна "Детали ингредиента" при клике на ингредиент')
-    @allure.description('''
-    Проверка открытия модального окна с деталями ингредиента:
-    1. Перейти в конструктор бургеров
-    2. Кликнуть на любой ингредиент в списке
-    3. Дождаться открытия модального окна с детальной информацией
-    4. Проверить отображение модального окна на странице
-    ''')
+    @allure.title('Проверка отображения модального окна "Детали ингредиента"')
     def test_displaying_modal_window_details_of_ingredient_success(self, driver):
         main_page = MainPage(driver)
+        
         main_page.click_on_ingredient()
         assert main_page.check_displaying_of_modal_details()
 
-    @allure.title('Проверка закрытия окна "Детали ингредиента" кликом по крестику')
-    @allure.description('''
-    Проверка закрытия модального окна с деталями ингредиента:
-    1. Открыть модальное окно с деталями ингредиента
-    2. Кликнуть на кнопку закрытия (крестик)
-    3. Модальное окно должно закрыться
-    4. Проверить отсутствие модального окна на странице
-    ''')
+    @allure.title('Проверка закрытия модального окна "Детали ингредиента" кликом по крестику')
     def test_close_modal_window_details_of_ingredient_success(self, driver):
         main_page = MainPage(driver)
-        main_page.click_on_ingredient()  # Открываем модальное окно
-        main_page.close_modal()  # Закрываем модальное окно
-        assert main_page.check_not_displaying_of_modal_details()
         
-    @allure.title('Проверка увеличения числа на счетчике при добавлении ингредиента в заказ')
-    @allure.description('''
-    Проверка работы счетчика ингредиентов при добавлении в заказ:
-    1. Проверить доступность ингредиентов и области заказа
-    2. Выполнить drag-and-drop ингредиента в заказ
-    3. Убедиться что конструктор продолжает работать корректно
-    4. Проверить отображение основных элементов после операции
-    ''')
+        main_page.click_on_ingredient()
+        main_page.close_modal()
+        assert main_page.check_not_displaying_of_modal_details()
+
+    @allure.title('Проверка увеличения счетчика при добавлении ингредиента в заказ')
     def test_changing_counter_for_ingredients_in_order_success(self, driver):
         main_page = MainPage(driver)
-    
-        # Проверяем что основные элементы конструктора доступны и отображаются
+        
+        # Получаем значение счетчика ДО добавления
+        initial_counter = main_page.get_ingredient_counter_value()
+        
+        # Выполняем drag and drop ингредиента в корзину
+        main_page.drag_and_drop_ingredient_to_order()
+        
+        # Получаем значение счетчика ПОСЛЕ добавления
+        new_counter = main_page.get_ingredient_counter_value()
+        
+        # Проверяем, что значение увеличилось
+        assert int(new_counter) > int(initial_counter)
         assert main_page.check_ingredient_displayed()
         assert main_page.check_basket_displayed()
-    
-        # Пробуем добавить ингредиент через drag-and-drop
-        main_page.drag_and_drop_ingredient_to_order()
-    
-        # Проверяем что страница продолжает работать корректно после операции
-        # Убеждаемся что основные элементы все еще доступны
-        assert main_page.check_ingredient_displayed()
-        assert main_page.check_constructor_button_displayed()
-
 
